f213e897d3884aaeca2313ec59f00835dd46f099
CMert Baykal <cmertbaykal@gmail.com>
2023-09-29 01:01:07 +0300

    [Cinemas][Compose and Google Maps Compose integration]

--- app/src/main/kotlin/com/adesso/movee/scene/cinemas/CinemasFragment.kt
+++ app/src/main/kotlin/com/adesso/movee/scene/cinemas/CinemasFragment.kt
@@ -1,11 +1,30 @@
 package com.adesso.movee.scene.cinemas
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import com.adesso.movee.R
 import com.adesso.movee.base.BaseFragment
 import com.adesso.movee.databinding.FragmentCinemasBinding
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.rememberCameraPositionState
 
 class CinemasFragment : BaseFragment<CinemasViewModel, FragmentCinemasBinding>() {
 
     override val layoutId: Int get() = R.layout.fragment_cinemas
 
+    override fun initialize() {
+        super.initialize()
+        binder.layout.setContent {
+            val kocaeli = LatLng(40.7711717, 29.8933016)
+            val cameraPositionState = rememberCameraPositionState {
+                position = CameraPosition.fromLatLngZoom(kocaeli, 11f)
+            }
+            GoogleMap(
+                modifier = Modifier.fillMaxSize(),
+                cameraPositionState = cameraPositionState
+            )
+        }
+    }
 }
