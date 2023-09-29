46fb672dfd5f8e8f895060dea5bce5538eabd8bb
CMert Baykal <cmertbaykal@gmail.com>
2023-09-29 05:24:11 +0300

    [Cinemas][Marker and Info Window Design]

--- app/src/main/kotlin/com/adesso/movee/scene/cinemas/CinemasFragment.kt
+++ app/src/main/kotlin/com/adesso/movee/scene/cinemas/CinemasFragment.kt
@@ -1,20 +1,25 @@
 package com.adesso.movee.scene.cinemas
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.core.content.ContextCompat
 import com.adesso.movee.R
 import com.adesso.movee.base.BaseFragment
+import com.adesso.movee.data.remote.model.cinema.OSMObject
 import com.adesso.movee.databinding.FragmentCinemasBinding
 import com.adesso.movee.scene.cinemas.components.GoogleMapsComponent
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -26,30 +31,22 @@ class CinemasFragment : BaseFragment<CinemasViewModel, FragmentCinemasBinding>()
         super.initialize()
 
         binder.layout.setContent {
-            val cinemas by viewModel.cinemaList.observeAsState()
+            val cinemasState = viewModel.cinemaList.observeAsState()
+            val cinemaDialogState by remember { mutableStateOf<OSMObject?>(null) }
 
             Box(
                 modifier = Modifier.fillMaxSize()
             ) {
                 GoogleMapsComponent(
                     permissionsState = locationPermission,
+                    cinemasState = cinemasState,
                     onLocationChange = {
                         viewModel.getCinemasOnLocation(it)
-                    }
-                ) {
-                    cinemas?.forEach {
-                        val markerState = MarkerState(position = LatLng(it.lat, it.lon))
+                    },
+                    onMarkerClick = {
 
-                        Marker(
-                            state = markerState,
-                            title = it.name,
-                            onClick = {
-                                markerState.showInfoWindow()
-                                true
-                            }
-                        )
                     }
-                }
+                )
             }
         }
     }
--- app/src/main/kotlin/com/adesso/movee/scene/cinemas/components/GoogleMapsComponent.kt
+++ app/src/main/kotlin/com/adesso/movee/scene/cinemas/components/GoogleMapsComponent.kt
@@ -2,22 +2,43 @@ package com.adesso.movee.scene.cinemas.components
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Bitmap
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.adesso.movee.R
+import com.adesso.movee.data.remote.model.cinema.OSMObject
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.LocationSource
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
@@ -25,14 +46,18 @@ import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerInfoWindow
+import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
 @SuppressLint("MissingPermission")
 @Composable
 fun GoogleMapsComponent(
     permissionsState: Boolean,
+    cinemasState: State<List<OSMObject>?>,
     onLocationChange: (LatLng) -> Unit,
-    markers: @Composable () -> Unit
+    onMarkerClick: (OSMObject) -> Unit
 ) {
     val context = LocalContext.current
 
@@ -86,6 +111,60 @@ fun GoogleMapsComponent(
         properties = mapProperties,
         uiSettings = mapUiSettings,
         cameraPositionState = cameraPositionState,
-        content = markers,
+    ) {
+        cinemasState.value?.forEach { cinema ->
+            val markerState = MarkerState(position = LatLng(cinema.lat, cinema.lon))
+            val icon = bitmapDescriptor(context, R.drawable.ic_maps_marker)
+
+            MarkerInfoWindow(
+                state = markerState,
+                icon = icon,
+                title = cinema.name,
+                infoWindowAnchor = Offset(-0.6f, 0.0f),
+                onClick = {
+                    onMarkerClick.invoke(cinema)
+                    markerState.showInfoWindow()
+                    false
+                }
+            ) {
+                Box(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(bottom = 8.dp)
+                        .background(
+                            color = colorResource(R.color.charcoal_grey_2),
+                            shape = RoundedCornerShape(4.dp)
+                        )
+                        .padding(horizontal = 8.dp, vertical = 5.dp)
+                ) {
+                    Text(
+                        color = Color.White,
+                        fontSize = 13.sp,
+                        fontWeight = FontWeight.Medium,
+                        text = it.title ?: cinema.name,
+                    )
+                }
+            }
+        }
+    }
+}
+
+fun bitmapDescriptor(
+    context: Context,
+    vectorResId: Int
+): BitmapDescriptor? {
+
+    // retrieve the actual drawable
+    val drawable = ContextCompat.getDrawable(context, vectorResId) ?: return null
+    drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+    val bm = Bitmap.createBitmap(
+        drawable.intrinsicWidth,
+        drawable.intrinsicHeight,
+        Bitmap.Config.ARGB_8888
     )
+
+    // draw it onto the bitmap
+    val canvas = android.graphics.Canvas(bm)
+    drawable.draw(canvas)
+    return BitmapDescriptorFactory.fromBitmap(bm)
 }
